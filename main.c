205cd1fc89d1ccae85107b0de6af65767d328bd4
Fabio Rodrigues Pereira <macaira1466@Fabios-MacBook-Air.local>
2021-01-15 00:38:49 -0300

    files stored

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  /* preprocessor command */
+
+int main() {
+    /* content of the main function */
+    printf("Hello, World!\n");
+    printf("The size of int is %lu.\n", sizeof(int));
+    return 0;
+}
