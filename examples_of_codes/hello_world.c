e241be34573f7c08940135bfb73ae96d77fdf618
Fabio Rodrigues Pereira <macaira1466@Fabios-MacBook-Air.local>
2021-01-16 16:56:06 -0300

    added files

---
+++ examples_of_codes/hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   /* my first program in C */
+   printf("Hello, World! \n");
+   printf("This is my first program. \n");
+   
+   return 0;
+}
---
+++ examples_of_codes/int_test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+   printf("Storage size for int : %lu \n", sizeof(int));
+   
+   return 0;
+}
