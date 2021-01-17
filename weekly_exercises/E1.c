502b98f6e8d925347e4c88faa89d9cfca8dc7881
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-01-17 14:35:51 -0300

    exercise in progress

--- weekly_exercises/E1.c
+++ weekly_exercises/E1.c
@@ -6,7 +6,31 @@
 #include <stdlib.h>
 
 
-int exercise1a() {
+void pointers_example (){
+    int a, *p;
+    p = &a;
+    a = 100;
+    *p = 5;
+
+    // returns 5
+    printf("Pointer *p has changed the variable a to %d \n", a);
+}
+
+
+void array_example (){
+    int *p, i;
+    p = malloc(3 * sizeof(int));
+    *(p+0) = 2;
+    *(p+1) = 4;
+    *(p+2) = 6;
+
+    for ( i=0; i<3; ++i ) {
+        printf("array[%d] = %d \n", i, p[i]);
+    }
+}
+
+
+void exercise1a() {
 
     int a = 2;
     double limit = 1;
@@ -30,22 +54,20 @@ int exercise1a() {
     }
 
     if (limit == 4. / 5) {
-        printf("The limit %f is 4/5.\n", limit);
+        printf("The limit %f is 4/5. \n", limit);
 
     } else {
-        printf("The limit %f is not 4/5.\n", limit);
+        printf("The limit %f is not 4/5. \n", limit);
     }
-
-    return 0;
 }
 
 
-int exercise1b(int n, int seed) {
+void exercise1b(int n, int seed) {
 
     /* local variables */
     int array[n], i, max, min;
 
-/*    printf("Enter size of the array : ");
+/*  printf("Enter size of the array : ");
     scanf("%d",&n);*/
 
     /* setting the seed */
@@ -53,7 +75,7 @@ int exercise1b(int n, int seed) {
 
     for ( i = 0; i < n; ++i) {
         array[i] = rand();
-        printf( "array[%d] = %d\n", i, array[i]);
+        printf( "array[%d] = %d \n", i, array[i]);
     }
 
     min=max=array[0];
@@ -66,18 +88,33 @@ int exercise1b(int n, int seed) {
             max = array[i];
     }
 
-    printf("minimum of array is : %d", min);
-    printf("\nmaximum of array is : %d \n", max);
-
-    return 0;
+    printf("minimum of array is : %d \n", min);
+    printf("maximum of array is : %d \n", max);
 }
 
 
-int exercise1c(){
+int exercise1c(int m, int n, int seed){
+
+    int i, j, array[m][n];
+
+    /* setting the seed */
+    srand(seed);
+
+    for ( i=0; i<m; ++i ) {
+        for ( j=0; j<n; ++j ) {
+            array[i][j] = i+j;
+        }
+    }
+
+    printf("%d \n", array[0][0]);
+
     return 0;
 }
-
 
 int main(){
-    exercise1b( 10, 11);
+    pointers_example();  // returns 5
+    array_example();  // returns array
+    exercise1a();
+    exercise1b(3, 10);
+    // exercise1c();
 }
