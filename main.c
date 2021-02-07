5d7679477354c45d8386f7593d668970ecdcf69f
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-02-07 18:29:56 -0300

    exercises E2

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>  /* preprocessor command */
-
-int main() {
-    /* content of the main function */
-    printf("Hello, World!\n");
-    printf("The size of int is %lu.\n", sizeof(int));
-    return 0;
-}
---
+++ weekly_exercises/W2/E2.c
@@ -0,0 +1,13 @@
+// Author: Fabio Rodrigues Pereira
+// E-mail: fabior@uio.no
+// ./a.out
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdatomic.h>
+
+int main(){
+    return 0;
+};
