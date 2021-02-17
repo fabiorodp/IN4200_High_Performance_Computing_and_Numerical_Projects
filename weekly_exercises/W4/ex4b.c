fb0cb599c32fd5e653f2bce33f80fea661e8173b
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-02-17 11:05:56 -0300

    exercises done

---
+++ weekly_exercises/W4/ex4b.c
@@ -0,0 +1,48 @@
+// Author: Fabio Rodrigues Pereira
+// E-mail: fabior@uio.no
+
+// compiling & running
+// gcc ex4b.c
+// ./a.out
+
+#include <stdlib.h> // rand, malloc, calloc and free.
+#include <stdio.h>  // printf
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+    double arrA, arrB;
+    int N = 100, s = 10, stride=2, i;
+    double *A = malloc(N * sizeof *A);
+    double *B = calloc(N, sizeof *B);
+
+    // Assigning some random numbers:
+    srand(time(NULL)); // Seeds the random number generator.
+
+    // assigning random values to array B
+    for (i = 0; i < N; ++i)
+    {
+        B[i] = (double)rand();
+    } 
+
+    clock_t t0 = clock();
+    for (int i = 0; i < N ; i += stride)
+    {
+        A[i] = s*B[i];
+        //printf("%f\n", A[i]);
+    }
+    clock_t t1 = clock();
+    
+    double elapsed_time = 1000*(double)(t1 - t0)/CLOCKS_PER_SEC;
+    printf("Elapsed time: %lf ms\n", elapsed_time);
+
+    double bytes = 2 * N * sizeof *A;
+    printf("Bytes per sec: %f\n", bytes/elapsed_time);
+    printf("Flops per sec: %f\n", N/elapsed_time);
+
+    free(A);
+    free(B);
+
+    return 0;
+};
