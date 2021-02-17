64fd1862a8c0b63785a0db64e12d503c650ad6e1
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-02-16 22:57:56 -0300

    edited

---
+++ weekly_exercises/W3/ex3.c
@@ -0,0 +1,31 @@
+// Author: Fabio Rodrigues Pereira
+// E-mail: fabior@uio.no
+
+// compiling & running
+// gcc E3c.c
+// ./a.out
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>       /* clock_t clock(void); */
+/* printf("%d (%f seconds)",t,((float)t)/CLOCKS_PER_SEC); */
+//#include <stdatomic.h>
+
+//double pow100(double x);
+
+int main()
+{
+    // int power = 100;
+    
+    // // Exercise 3
+    // clock_t t0 = clock();
+    // float result = 1;
+    // for (int i = 1; i <= power; ++i) {
+    //     result *= result;
+    // }
+    // clock_t t1 = clock();
+    // printf("%f", (float)((t1-t0)/CLOCKS_PER_SEC);
+    printf("oi");
+    return 0
+};
