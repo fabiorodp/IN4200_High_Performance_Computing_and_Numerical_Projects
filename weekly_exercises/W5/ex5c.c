fe6d6a313f81386c797907d0277bdf6b56aaaf1f
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-02-23 23:47:25 -0300

    Exercise initialized

---
+++ weekly_exercises/W5/ex5c.c
@@ -0,0 +1,17 @@
+// Author: Fabio Rodrigues Pereira
+// E-mail: fabior@uio.no
+
+// compiling & running
+// gcc ex5c.c
+// ./a.out
+// gcc w5_e3_optimize.c week5.h
+
+#include <stdlib.h> // rand, malloc, calloc and free.
+#include <stdio.h>  // printf
+#include <math.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+
+}
