ef6b0c0f66d4de822bb13a9393ecf4c8a4c4480f
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-03-29 12:25:24 -0300

    major fixes

--- exam1/main.c
+++ exam1/main.c
@@ -32,39 +32,60 @@ void test_read_and_create_SNN_graph1(char *filename)
     int **SNN_table;
 
     // calling the function to read file and return table2D and N
-    read_graph_from_file1(filename, &N, &table2D);
-
-    printf("Creating SNN graph 1 without OMP...\n");
-    // calling the function to return SNN_table
+    printf("Reading Connectivity Graph 1...\n");
     clock_t t0 = clock();
-    create_SNN_graph1(N, table2D, &SNN_table);
+    read_graph_from_file1(filename, &N, &table2D);
     clock_t t1 = clock();
 
     double elapsed_time = (double)(t1-t0)/CLOCKS_PER_SEC;
-    printf(">> Elapsed time: %f s.\n", elapsed_time);
+    printf(">> CPU time: %f s.\n", elapsed_time);
+
+    // checking if the user want to print table2D
+    printf("Please, type 'y' if you want to print table2D here:");
+    printing_table2D(table2D, N);
+
+    // calling the function to return SNN_table
+    printf("Creating SNN graph 1 without OMP...\n");
+    double tt0 = omp_get_wtime();
+    t0 = clock();
+    create_SNN_graph1(N, table2D, &SNN_table);
+    t1 = clock();
+    double tt1 = omp_get_wtime();
+
+    elapsed_time = (double)(t1-t0)/CLOCKS_PER_SEC;
+    double elapsed_time1 = tt1-tt0;
+    printf(">> CPU time: %f s && ELAPSED Time: %f s.\n", elapsed_time, elapsed_time1);
 
     // declaration for the SNN_table_omp
     int **SNN_table_omp;
 
-    printf("Creating SNN graph 1 with OMP...\n");
     // calling the function to return SNN_table
+    printf("Creating SNN graph 1 with OMP...\n");
+    tt0 = omp_get_wtime();
     t0 = clock();
     create_SNN_graph1_omp(N, table2D, &SNN_table_omp);
     t1 = clock();
+    tt1 = omp_get_wtime();
 
-    double elapsed_time1 = (double)(t1-t0)/CLOCKS_PER_SEC;
-    printf(">> Elapsed time: %f s.\n", elapsed_time1);
+    double elapsed_time_omp = (double)(t1-t0)/CLOCKS_PER_SEC;
+    double elapsed_time_omp1 = tt1-tt0;
+    printf(">> CPU time: %f s && ELAPSED Time: %f s.\n", elapsed_time_omp, elapsed_time_omp1);
 
-    // printing speedup
-    printf("Speedup (SNN_table_omp/SNN_table):\n>> %f\n",
-           elapsed_time1/elapsed_time);
+    // Speedup between SNN_table and SNN_table_omp
+    printf("Speedup between SNN_table and SNN_table_omp:\n>> SNN_table/SNN_table_omp = %f",
+           elapsed_time1/elapsed_time_omp1);
 
     // comparing if SNN_table and SNN_table_omp are equals
-    printf("Comparing if SNN_table and SNN_table_omp are equals:\n");
+    printf("\nComparing if SNN_table and SNN_table_omp are equals:\n");
     comparing_2D_tables(SNN_table, SNN_table_omp, N);
 
     // checking if the user want to print SNN_table
+    printf("Please, type 'y' if you want to print SNN_table here:");
     printing_SNN_table(SNN_table, N);
+
+    // checking if the user want to print SNN_table_omp
+    printf("Please, type 'y' if you want to print SNN_table_omp here:");
+    printing_SNN_table(SNN_table_omp, N);
 
     // freeing memory
     free(*table2D);
@@ -80,39 +101,60 @@ void test_read_and_create_SNN_graph2(char *filename, int *N, int **col_idx,
                                      int **row_ptr, int **SNN_val)
 {
     // calling the function to read file and return N, row_ptr and col_idx
-    read_graph_from_file2(filename, &(*N), &(*row_ptr), &(*col_idx));
-
-    printf("Creating SNN graph 2 without OMP...\n");
-    // calling the function to return SNN_val
+    printf("Reading Connectivity Graph 2...\n");
     clock_t t0 = clock();
-    create_SNN_graph2((*N), (*row_ptr), (*col_idx), &(*SNN_val));
+    read_graph_from_file2(filename, &(*N), &(*row_ptr), &(*col_idx));
     clock_t t1 = clock();
 
     double elapsed_time = (double)(t1-t0)/CLOCKS_PER_SEC;
-    printf(">> Elapsed time: %f s.\n", elapsed_time);
+    printf(">> CPU time: %f s.\n", elapsed_time);
+
+    // checking if the user want to print col_idx and row_ptr:
+    printf("Please, type 'y' if you want to print col_idx and row_ptr here:");
+    printing_CRS((*col_idx), (*row_ptr), (*N));
+
+    // calling the function to return SNN_val
+    printf("\nCreating SNN graph 2 without OMP...\n");
+    double tt0 = omp_get_wtime();
+    t0 = clock();
+    create_SNN_graph2((*N), (*row_ptr), (*col_idx), &(*SNN_val));
+    t1 = clock();
+    double tt1 = omp_get_wtime();
+
+    elapsed_time = (double)(t1-t0)/CLOCKS_PER_SEC;
+    double elapsed_time1 = tt1-tt0;
+    printf(">> CPU time: %f s && ELAPSED Time: %f s.\n", elapsed_time, elapsed_time1);
 
     // declaration for the SNN_val
     int *SNN_val_omp;
 
-    printf("Creating SNN graph 2 with OMP...\n");
     // calling the function to return SNN_val
+    printf("Creating SNN graph 2 with OMP...\n");
+    tt0 = omp_get_wtime();
     t0 = clock();
     create_SNN_graph2_omp((*N), (*row_ptr), (*col_idx), &SNN_val_omp);
     t1 = clock();
+    tt1 = omp_get_wtime();
 
-    double elapsed_time1 = (double)(t1-t0)/CLOCKS_PER_SEC;
-    printf(">> Elapsed time: %f s.\n", elapsed_time1);
+    double elapsed_time_omp = (double)(t1-t0)/CLOCKS_PER_SEC;
+    double elapsed_time_omp1 = tt1-tt0;
+    printf(">> CPU time: %f s && ELAPSED Time: %f s.\n", elapsed_time_omp, elapsed_time_omp1);
 
-    // printing speedup
-    printf("Speedup (SNN_val_omp/SNN_val):\n>> %f\n",
-           elapsed_time1/elapsed_time);
+    // Speedup between SNN_val and SNN_val_omp
+    printf("Speedup between SNN_val and SNN_val_omp:\n>> SNN_val/SNN_val_omp = %f",
+           elapsed_time1/elapsed_time_omp1);
 
     // Comparing if SNN_val and SNN_val_omp are equals
-    printf("Comparing if SNN_val and SNN_val_omp are equals:\n");
+    printf("\nComparing if SNN_val and SNN_val_omp are equals:\n");
     comparing_SNN_val((*SNN_val), SNN_val_omp, (*row_ptr)[*N+1]);
 
     // checking if the user want to print SNN_val
+    printf("Please, type 'y' if you want to print SNN_val here:");
     printing_SNN_val((*SNN_val), (*row_ptr)[*N+1]);
+
+    // checking if the user want to print SNN_val
+    printf("\nPlease, type 'y' if you want to print SNN_val here:");
+    printing_SNN_val(SNN_val_omp, (*row_ptr)[*N+1]);
 
     free(SNN_val_omp);
 }
@@ -132,8 +174,77 @@ void test_check_node(int N, int *col_idx, int *row_ptr, int *SNN_val)
 }
 
 
+/*
+This is an example of the output using the data/graph_example.txt:
+
+% ./a.out data/graph_example.txt 4
+
+Testing read_graph_from_file1.c and create_SNN_graph1.c.....
+Reading Connectivity Graph 1...
+>> CPU time: 0.000079 s.
+Please, type 'y' if you want to print table2D here:y
+Printing...
+0 1 1 1 0
+1 0 1 1 0
+1 1 0 1 1
+1 1 1 0 1
+0 0 1 1 0
+Creating SNN graph 1 without OMP...
+>> CPU time: 0.000019 s && ELAPSED Time: 0.000021 s.
+Creating SNN graph 1 with OMP...
+>> CPU time: 0.001021 s && ELAPSED Time: 0.000510 s.
+Speedup between SNN_table and SNN_table_omp:
+>> SNN_table/SNN_table_omp = 0.041141
+Comparing if SNN_table and SNN_table_omp are equals:
+>> True
+Please, type 'y' if you want to print SNN_table here:y
+Printing...
+0 2 2 2 0
+2 0 2 2 0
+2 2 0 3 1
+2 2 3 0 1
+0 0 1 1 0
+Please, type 'y' if you want to print SNN_table_omp here:y
+Printing...
+0 2 2 2 0
+2 0 2 2 0
+2 2 0 3 1
+2 2 3 0 1
+0 0 1 1 0
+
+Testing read_graph_from_file2.c and create_SNN_graph2.c.....
+Reading Connectivity Graph 2...
+>> CPU time: 0.000229 s.
+Please, type 'y' if you want to print col_idx and row_ptr here:y
+Printing col_idx...
+1 2 3 0 2 3 0 1 3 4 0 1 2 4 2 3
+Printing row_ptr...
+0 3 6 10 14 16
+Creating SNN graph 2 without OMP...
+>> CPU time: 0.000008 s && ELAPSED Time: 0.000009 s.
+Creating SNN graph 2 with OMP...
+>> CPU time: 0.000273 s && ELAPSED Time: 0.000114 s.
+Speedup between SNN_val and SNN_val_omp:
+>> SNN_val/SNN_val_omp = 0.079498
+Comparing if SNN_val and SNN_val_omp are equals:
+>> True
+Please, type 'y' if you want to print SNN_val here:y
+Printing SNN_val...
+2 2 2 2 2 2 2 2 3 1 2 2 3 1 1 1
+Please, type 'y' if you want to print SNN_val here:y
+Printing SNN_val...
+2 2 2 2 2 2 2 2 3 1 2 2 3 1 1 1
+
+Testing check_node.c......................................
+Please, give the node_id number:4
+Now, the tau number:1
+>> The Nodes that form a cluster with node_id 4 and tau 1: 0,1,3,2
+*/
 int main(int argc, char *argv[])
 {
+    // setting the number of threads
+    omp_set_num_threads(atoi(argv[2]));
+
     printf("\nTesting read_graph_from_file1.c and create_SNN_graph1.c.....\n");
     test_read_and_create_SNN_graph1(argv[1]);
 
--- exam1/main.h
+++ exam1/main.h
@@ -6,15 +6,33 @@
 #include <stdio.h>  // printf, scanf
 
 
-void printing_SNN_table(int **SNN_table, int N)
+void printing_table2D(char **SNN_table, int N)
 {
     char ipt = 'n';
-    printf("Please, type 'y' if you want to print SNN_table here:");
     scanf("%c", &ipt); getchar();
 
     if ( ipt=='y' )
     {
-        printf("Printing SNN_table...\n");
+        printf("Printing...\n");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j=0; j < N; j++)
+                printf("%d ", SNN_table[i][j]);
+
+            printf("\n");
+        }
+    }
+}
+
+
+void printing_SNN_table(int **SNN_table, int N)
+{
+    char ipt = 'n';
+    scanf("%c", &ipt); getchar();
+
+    if ( ipt=='y' )
+    {
+        printf("Printing...\n");
         for (int i = 0; i < N; i++)
         {
             for (int j=0; j < N; j++)
@@ -40,13 +58,12 @@ void comparing_2D_tables(int **SNN_table, int **SNN_table_omp, int N)
 void printing_SNN_val(int *SNN_val, int length)
 {
     char ipt = 'n';
-    printf("Please, type 'y' if you want to print SNN_val here:");
     scanf("%c", &ipt); getchar();
 
     if (ipt=='y')
     {
         // testing returned values for SNN_val
-        printf("Printing SNN_val...\n");
+        printf("Printing...\n");
         for ( size_t i = 0; i < length; i++)
             printf("%d ", SNN_val[i]);
     }
@@ -60,4 +77,24 @@ void comparing_SNN_val(int *SNN_val, int *SNN_val_omp, int length)
         if (SNN_val[i] != SNN_val_omp[i]) is_equal=0;
 
     printf(is_equal==1 ? ">> True\n" : ">> False\n");
+}
+
+
+void printing_CRS(int *col_idx, int *row_ptr, int N)
+{
+    char ipt = 'n';
+    scanf("%c", &ipt); getchar();
+
+    if ( ipt=='y' )
+    {
+        // testing returned values for col_idx
+        printf("Printing col_idx...\n");
+        for ( size_t i = 0; i < row_ptr[N+1]; i++)
+            printf("%d ", col_idx[i]);
+
+        // testing returned values for row_ptr
+        printf("\nPrinting row_ptr...\n");
+        for ( size_t i = 0; i < N+1; i++)
+            printf("%d ", row_ptr[i]);
+    }
 }
