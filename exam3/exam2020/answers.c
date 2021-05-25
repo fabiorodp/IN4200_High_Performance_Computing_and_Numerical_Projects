bb923421f96823fd30616a0d0dbbb4f301f5f8a8
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2021-05-25 17:48:03 +0200

    in progress

---
+++ exam3/exam2020/answers.c
@@ -0,0 +1,17 @@
+// compiling & running
+// clang -Xpreprocessor -fopenmp answers.c -lomp
+// ./a.out
+
+#include <stdlib.h> // rand, malloc, calloc, realloc and free.
+#include <stdio.h>  // printf, scanf
+#include <mpi.h>
+#include <math.h>
+#include <omp.h>
+#include "string.h"
+#include <mpi.h>
+
+
+int main( int argc, char **argv )
+{
+
+}
